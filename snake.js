46bd407eff85bfb27a69d192391f0cca75e699c8
Mike Belinskiy <mikhail.belinskiy@gmail.com>
2022-06-25 16:53:42 +0300

    Add files via upload

--- snake.js
+++ snake.js
@@ -1,4 +1,39 @@
+ 
+ class Snake{
+    constructor(x, y, size){
+        this.x = x
+        this.y = y
+        this.size = size
+        this.tail = [{x:this.x, y:this.y}]
+        this.rotateX = 0
+        this.rotateY = 1
+    }
 
+    move(){
+            var newReact;
+            if(this.rotateX == 1){
+                newReact = {
+                        x: this.tail[this.tail.length -1].x + this.size,
+                        y: this.tail[this.tail.length -1].y
+                }
+            }else if(this.rotateX == -1){
+                newReact = {
+                        x: this.tail[this.tail.length -1].x - this.size,
+                        y: this.tail[this.tail.length -1].y
+                }
+            }else if(this.rotateY ==1){
+                newReact = {
+                        x: this.tail[this.tail.length -1].x,
+                        y: this.tail[this.tail.length -1].y + this.size
+                }
+            }else if(this.rotateY == -1){
+                newReact = {
+                        x: this.tail[this.tail.length -1].x,
+                        y: this.tail[this.tail.length -1].y - this.size
+                }
+            }
+        }   
+    }
 
 
 
