0c07f67232a1a457ffe35cc7633b6164b2e2e2c1
Mike Belinskiy <mikhail.belinskiy@gmail.com>
2022-06-26 23:57:32 +0300

    Add files via upload

--- snake.js
+++ snake.js
@@ -32,9 +32,27 @@
                         y: this.tail[this.tail.length -1].y - this.size
                 }
             }
+
+            this.tail.shift()
+            this.tail.push(newReact)
         }   
     }
 
+    class Apple{
+            constructor(){
+                var isTouching;
+                while(true){
+                    isTouching = false; 
+                    this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size
+                    this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size
+                    for(var i = 0; i < snake.tail.length; i++){
+                         if(this.x == snake.tail[i].x && this.y == snake.tail[i].y){
+                             isTouching = true
+                         }
+                    }
+                }
+            }
+    }
 
 
 var canvas = ducoument.getElementById("canvas")
