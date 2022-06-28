964c82ed320341b3d3fa8099f8027d14dd0f147e
Mike Belinskiy <mikhail.belinskiy@gmail.com>
2022-06-28 23:52:33 +0300

    Add files via upload

--- snake.js
+++ snake.js
@@ -92,4 +92,13 @@ function draw(){
         createReact(snake.tail[i].x + 2.5, snake.tail[i].y +2.5)
             snake.size -5, snake.size -5, 'white'
     }
+    canvasContext.font = "20px Arial"
+    canvasContext.fillStyle = "#00FF42"
+    canvasContext.fillText("Score: ", (snake.tail.length +1), canvas.width -120, 18); 
+    createReact(apple.x, apple.y, apple.size, apple.color)
+}
+
+function createReact(x, y, width, height, color){
+    canvasContext.fillStyle = color
+    canvasContext.fillReact (x, y, width, height, color)
 }
